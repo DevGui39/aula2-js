45f35a150d0da004c1c0e0c93dfeb21f54410723
Guilherme Gomes <guilherme.gomesesteves@gmail.com>
2024-06-19 16:44:06 -0300

    adicionado exercicio

---
+++ index.js
@@ -0,0 +1,12 @@
+let corPref = prompt("inofrme sua cor favorita")
+
+alert(`${corPref} é muito bonito.`)
+
+
+
+let livrosEstoque = 100
+let addLivros = prompt("Quantos Livros gostaria de adicionar no estoque")
+const livrosTotais = livrosEstoque + parseInt(addLivros)
+
+alert(`${livrosTotais} essa é a nova quantidade de livros no estoque!`)
+
